8fd33f563aa7f4bd00e184305e1c284edbe50dd2
leo8459 <joseaguilar987654321@gmail.com>
2024-09-08 23:35:53 -0400

    Update 2024_06_07_135921_create_solicitudes_table.php

--- database/migrations/2024_06_07_135921_create_solicitudes_table.php
+++ database/migrations/2024_06_07_135921_create_solicitudes_table.php
@@ -22,33 +22,29 @@ return new class extends Migration
             $table->foreignId('direccion_id')->nullable()->constrained('direcciones');
             $table->foreignId('encargado_id')->nullable()->constrained('encargados');
             $table->foreignId('encargado_regional_id')->nullable()->constrained('encargados');
-
-            $table->string('guia')->nullable();
+            $table->string('guia');
             $table->integer('estado')->default(1);
             $table->string('peso_o')->nullable();
             $table->string('peso_v')->nullable();
             $table->string('peso_r')->nullable();
-            $table->string('remitente')->nullable();
-            $table->string('telefono')->nullable();
-            $table->string('contenido')->nullable();
-            $table->string('destinatario')->nullable();
-            $table->string('telefono_d')->nullable();
+            $table->string('remitente');
+            $table->string('telefono');
+            $table->string('contenido');
+            $table->string('destinatario');
+            $table->string('telefono_d');
             $table->string('direccion_d')->nullable();
-            $table->string('direccion_especifica_d')->nullable();
-            $table->string('zona_d')->nullable();
+            $table->string('direccion_especifica_d');
+            $table->string('zona_d');
             $table->string('ciudad')->nullable();
             $table->string('nombre_d')->nullable();
             $table->string('fecha')->nullable();
             $table->string('fecha_d')->nullable();
             $table->string('fecha_recojo_c')->nullable();
             $table->string('fecha_devolucion')->nullable();
-            $table->string('fecha_envio_regional')->nullable();
-            
+            $table->string('fecha_envio_regional')->nullable();            
             $table->string('reencaminamiento')->nullable();
             $table->int('peso_reencaminar')->nullable();
-            $table->int('peso_reencaminado_recibido')->nullable();
-
-            
+            $table->int('peso_reencaminado_recibido')->nullable();            
             $table->string('observacion')->nullable();
             $table->string('justificacion')->nullable();
             $table->text('imagen')->nullable();
